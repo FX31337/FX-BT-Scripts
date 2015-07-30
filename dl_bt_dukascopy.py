0d0b3789a5c9f04f2fd6cbda6d2b80755711b6a2
Rafal <kenorb@gmail.com>
2015-07-30 15:15:16 +0100

    Adding binary to CSV conversion.

--- dl_bt_dukascopy.py
+++ dl_bt_dukascopy.py
@@ -10,6 +10,8 @@ import time
 import urllib.request
 from urllib.error import HTTPError,ContentTooShortError
 import lzma
+from struct import *
+import csv
 
 # Create a mapping of currencies.
 all_currencies = {
@@ -55,14 +57,18 @@ class Dukascopy:
 
     def __init__(self, pair, year, month, day, hour, dest = "download"):
         if not os.path.exists(dest):
-            os.mkdir(dest)
+            os.makedirs(dest)
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
         self.url = self.url_tpl % (pair, int(year), month, day, hour)
         self.path = "%s/%04d-%02d-%02d--%02dh_ticks.bi5" % (dest, year, month, day, hour)
 
     def download(self):
         print("Downloading %s into: %s..." % (self.url, self.path))
         if os.path.isfile(self.path):
-            print("File exists.");
+            print("File (%s) exists, so skipping." % (self.path));
             return True
         else:
             try:
@@ -72,30 +78,60 @@ class Dukascopy:
                     print("Error: %s, reason: %s." % (err.code, err.reason));
                     return False
             except ContentTooShortError as err:
-                print("Error: The downloaded data is less than the expected amount, so ignoring.")
+                print("Error: The downloaded data is less than the expected amount, so skipping.")
                 return False
         return True
 
     def bt5_to_csv(self):
-        with open(self.path, 'r') as f:
-            data = f.read()
+        fileSize = os.stat(self.path).st_size
+        if fileSize == 0:
+            return
+
         new_path = self.path.replace("bi5", "csv")
+        if os.path.isfile(new_path):
+            print("CSV file (%s) exists, so skipping." % (new_path));
+
         print("Converting into CSV (%s)..." % (new_path))
-        # todo
-        output = lzma.decompress(data)
-        # todo
-        with open(new_path, 'w') as f:
-            f.write(output)
+
+        # Opening, uncompressing & reading raw data
+        with lzma.open(self.path) as f:
+            data = f.read()
+
+        # Opening output CSV file for write
+        f = open(new_path, 'w', newline='')
+        w = csv.writer(f, quoting = csv.QUOTE_NONE)
+
+        for i in range(0, len(data)//20):
+            row = bytearray()
+            for j in range(0, 20):
+                row.append(data[i*20 + j])
+
+            # Big-endian to Little-endian conversion
+            row = unpack('>iiiff', row)
+
+            # Calculating & formatting column values
+            minute = row[0]/1000//60
+            second = row[0]/1000 - minute*60
+            timestamp = "%d.%02d.%02d %02d:%02d:%06.3f" % (self.year, self.month, self.day, self.hour, minute, second)
+            bidPrice = row[2]/1e5
+            askPrice = row[1]/1e5
+            bidVolume = "%.2f" % (row[4])
+            askVolume = "%.2f" % (row[3])
+
+            # Writing one row in CSV format
+            w.writerow([timestamp, bidPrice, askPrice, bidVolume, askVolume])
+        f.close()
 
 if __name__ == '__main__':
 
     # Parse arguments.
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("-?", "--help",         action="help",                         help="Show this help message and exit." )
-    parser.add_argument("-v", "--verbose",      action="store_true", dest="verbose",   help="Increase output verbosity." )
-    parser.add_argument("-d", "--download-dir", action="store",      dest="dest",      help="Directory to download files.", default="download")
-    parser.add_argument("-p", "--pairs",        action="store",      dest="pairs",     help="Pair(s) to download (separated by comma).", default="all")
-    parser.add_argument("-y", "--years",        action="store",      dest="years",     help="Year(s) to download (separated by comma).", default="all")
+    parser.add_argument("-?", "--help",         action="help",                          help="Show this help message and exit." )
+    parser.add_argument("-v", "--verbose",      action="store_true",  dest="verbose",   help="Increase output verbosity." )
+    parser.add_argument("-d", "--download-dir", action="store",       dest="dest",      help="Directory to download files.", default="download")
+    parser.add_argument("-c", "--csv-convert",  action="store_true",  dest="csv",       help="Perform CSV conversion.")
+    parser.add_argument("-p", "--pairs",        action="store",       dest="pairs",     help="Pair(s) to download (separated by comma).", default="all")
+    parser.add_argument("-y", "--years",        action="store",       dest="years",     help="Year(s) to download (separated by comma).", default="all")
     args = parser.parse_args()
 
     curr_year = datetime.date.today().year
@@ -115,7 +151,8 @@ if __name__ == '__main__':
                                 if unix > all_currencies.get(pair) and unix < time.time(): # Validate dates.
                                     ds = Dukascopy(pair, int(year), month, day, hour, dest=args.dest + "/" + pair)
                                     ds.download()
-                                    #ds.bt5_to_csv() #todo
+                                    if args.csv:
+                                        ds.bt5_to_csv()
                                     #raise KeyboardInterrupt # perform one record for testing
                             except ValueError: # Ignore invalid dates.
                                 continue
