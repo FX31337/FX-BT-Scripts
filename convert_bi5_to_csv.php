2ba01b414104df024455bc361853c8617190b3ca
Rafal <kenorb@gmail.com>
2015-09-10 00:10:37 +0100

    Added more symbols, added license and translated Chinese comments.

--- convert_bi5_to_csv.php
+++ convert_bi5_to_csv.php
@@ -1,4 +1,19 @@
 <?php
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // Usage:
 //   php convert_bi5_to_csv.php EURUSD start_date end_date foo.csv
 // E.g.: php convert_bi5_to_csv.php EURUSD 201001 201101 EURUSD_2010_01-2011_01.csv
