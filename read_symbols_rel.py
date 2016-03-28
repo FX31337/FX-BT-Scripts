22e0dc5ae4dc414c7e4e2321357cf439ddf38c05
LemonBoy <thatlemon@gmail.com>
2016-03-28 19:22:49 +0200

    Introduce a nicer harness to parse the binary data

--- read_symbols_rel.py
+++ read_symbols_rel.py
@@ -3,8 +3,72 @@ import datetime
 import argparse
 import sys
 
-record_length = 128
+def get_fields_size(spec):
+    fmt_str = ''.join(x[1] for x in spec)
+    return struct.calcsize(fmt_str)
 
+class BStruct():
+    def __init__(self, buf, offset = 0):
+        for (name, fmt, *rest) in self._fields:
+            field_size = struct.calcsize(fmt)
+            val = struct.unpack_from(self._endianness + fmt, buf, offset)
+
+            # Flatten the single-element arrays
+            if type(val) is tuple and len(val) == 1:
+                val = val[0]
+
+            setattr(self, name, val)
+
+            offset += field_size
+
+    def __str__(self):
+        ret = ''
+
+        for (name, _, *fmt) in self._fields:
+            val_repr = getattr(self, name)
+
+            # Pretty print the value using the custom formatter
+            if len(fmt):
+                pp, = fmt
+                val_repr = pp(self, getattr(self, name))
+
+            ret += '{} = {}\n'.format(name, val_repr)
+
+        return ret
+
+def pretty_print_time(obj, x):
+    return datetime.datetime.fromtimestamp(x)
+
+def pretty_print_string(obj, x):
+    return x.decode('utf-8')
+
+class SymbolSel(BStruct):
+    _endianness = '<'
+    _fields = [
+            ('symbol', '12s', pretty_print_string),
+            ('digits', 'I'),
+            ('index', 'I'),
+            ('unknown_1', 'I'),
+            ('group', 'I'),
+            ('unknown_2', 'I'),
+            ('pointSize', 'd'),
+            ('spread', 'I'),
+            ('unknown_3', 'I'),
+            ('tickType', 'I'),
+            ('unknown_4', 'H'),
+            ('unknown_5', 'H'),
+            ('time', 'I', pretty_print_time),
+            ('unknown_6', 'I'),
+            ('bid', 'd'),
+            ('ask', 'd'),
+            ('sessionHigh', 'd'),
+            ('sessionLow', 'd'),
+            ('unknown_17', '16c'),
+            ('bid_2', 'd'),
+            ('ask_2', 'd')
+            ]
+    _size = get_fields_size(_fields)
+    assert(_size == 128)
 def dump_content(filename):
     try:
         fp = open(filename, 'rb')
@@ -15,58 +79,13 @@ def dump_content(filename):
     magic = fp.read(4)
 
     while True:
-        buf = fp.read(record_length)
+        buf = fp.read(SymbolSel._size)
 
-        if len(buf) != record_length:
+        if len(buf) != SymbolSel._size:
             break
 
-        symbol = buf[:12].decode('utf-8')
-        fields = struct.unpack('<IIIIIdIIIHHIIdddd4Idd', buf[12:])
-
-        sym_table = [
-                'digits',
-                'index',
-                None,
-                'group',
-                None,
-                'pointSize',
-                'spread',
-                None,
-                ['tickType', lambda x: ['uptick', 'downtick', 'n/a'][x]],
-                None,
-                None,
-                ['time', lambda x: datetime.datetime.fromtimestamp(x)],
-                None,
-                'bid',
-                'ask',
-                'sessionHigh',
-                'sessionLow',
-                None,
-                None,
-                None,
-                None,
-                'bid_2',
-                'ask_2',
-                ]
-
-        out = 'Symbol: {}\n'.format(symbol)
-
-        for (i, obj) in enumerate(fields):
-            handler = sym_table[i]
-
-            if handler == None:
-                continue
-
-            if type(handler) is list:
-                name = handler[0]
-                val = handler[1](obj)
-            else:
-                name = handler
-                val = obj
-
-            out += '{}: {}\n'.format(name, val)
-
-        print(out)
+        obj = SymbolSel(buf)
+        print(obj)
 
 if __name__ == '__main__':
     # Parse the arguments
