c0abdb5680b3e52b6d905179befe23dddabfda76
LemonBoy <thatlemon@gmail.com>
2016-03-28 19:59:10 +0200

    Add support for symbols.raw format

--- read_symbols.py
+++ read_symbols.py
@@ -36,12 +36,28 @@ class BStruct():
 
         return ret
 
+#
+# Pretty printers
+#
 def pretty_print_time(obj, x):
     return datetime.datetime.fromtimestamp(x)
 
 def pretty_print_string(obj, x):
     return x.decode('utf-8')
 
+def pretty_print_ignore(obj, x):
+    return '<...>'
+
+def pretty_print_hex(obj, x):
+    return '{:08x}'.format(x)
+
+def pretty_print_compact(obj, x):
+    if any(x): return x
+    return '[\\x00] * {}'.format(len(x))
+
+#
+# Structure definitions
+#
 class TicksRaw(BStruct):
     _endianness = '<'
     _fields = [
@@ -76,7 +92,7 @@ class SymbolSel(BStruct):
             ('ask', 'd'),
             ('sessionHigh', 'd'),
             ('sessionLow', 'd'),
-            ('unknown_17', '16c'),
+            ('unknown_17', '16s', pretty_print_compact),
             ('bid_2', 'd'),
             ('ask_2', 'd')
             ]
@@ -93,7 +109,52 @@ class Symgroups(BStruct):
     _size = get_fields_size(_fields)
     assert(_size == 80)
 
+class SymbolsRaw(BStruct):
+    _endianness = '<'
+    _fields = [
+            ('name', '12s', pretty_print_string),
+            ('description', '64s', pretty_print_string),
+            ('altName', '12s', pretty_print_string),
+            ('baseCurrency', '12s', pretty_print_string),
+            ('group', 'I'),
+            ('digits', 'I'),
+            ('tradeMode', 'I'),
+            ('backgroundColor', 'I', pretty_print_hex),
+            ('id', 'I'),
+            ('unknown_1', '1508c', pretty_print_ignore),
+            ('unknown_2', 'I'),
+            ('unknown_3', '8c'),
+            ('unknown_4', 'd'),
+            ('unknown_5', '12s', pretty_print_compact),
+            ('spread', 'I'),
+            ('unknown_6', '16s', pretty_print_compact),
+            ('swapLong', 'd'),
+            ('swapShort', 'd'),
+            ('unknown_7', 'I'),
+            ('unknown_8', 'I'),
+            ('contractSize', 'd'),
+            ('unknown_9', '16s', pretty_print_compact),
+            ('stopDistance', 'I'),
+            ('unknown_10', '12s', pretty_print_compact),
+            ('marginInit', 'd'),
+            ('marginMaintenance', 'd'),
+            ('marginHedged', 'd'),
+            ('marginDivider', 'd'),
+            ('pointSize', 'd'),
+            ('pointsPerUnit', 'd'),
+            ('unknown_11', '24s', pretty_print_compact),
+            ('marginCurrency', '12s', pretty_print_string),
+            ('unknown_12', '104s', pretty_print_ignore),
+            ('unknown_13', 'I'),
+            ]
+    _size = get_fields_size(_fields)
+    assert(_size == 1936)
+
 def dump_content(filename, offset, strucc):
+    """
+    Dump the content of the file "filename" starting from offset and using the
+    BStruct subclass pointed by strucc
+    """
     try:
         fp = open(filename, 'rb')
     except OSError as e:
@@ -115,14 +176,17 @@ if __name__ == '__main__':
     # Parse the arguments
     argumentParser = argparse.ArgumentParser(add_help=False)
     argumentParser.add_argument('-i', '--input-file', action='store', dest='inputFile', help='input file', required=True)
-    argumentParser.add_argument('-t', '--input-type', action='store', dest='inputType', help='input type, either sel,raw or symgroups', required=True)
+    argumentParser.add_argument('-t', '--input-type', action='store', dest='inputType', help='input type', required=True)
     argumentParser.add_argument('-h', '--help', action='help', help='Show this help message and exit')
     args = argumentParser.parse_args()
 
     if args.inputType == 'sel':
+        # There's a 4-byte magic preceding the data
         dump_content(args.inputFile, 4, SymbolSel)
-    elif args.inputType == 'raw':
+    elif args.inputType == 'ticksraw':
         dump_content(args.inputFile, 0, TicksRaw)
+    elif args.inputType == 'symbolsraw':
+        dump_content(args.inputFile, 0, SymbolsRaw)
     elif args.inputType == 'symgroups':
         dump_content(args.inputFile, 0, Symgroups)
     else:
