e4b2e22a9ea3186b9052c9f8bd7f9203db5974cd
Lyndon Gingerich <gundericusthemighty@gmail.com>
2021-03-20 21:55:11 -0500

    Removed extra whitespace

--- convert_csv_to_mt.py
+++ convert_csv_to_mt.py
@@ -81,7 +81,7 @@ def string_to_timestamp(s):
 class CSV(Input):
     def __init__(self, path):
         super().__init__(path)
-        self._map_obj = mmap.mmap(self.path.fileno(), 0, access=mmap.ACCESS_READ) 
+        self._map_obj = mmap.mmap(self.path.fileno(), 0, access=mmap.ACCESS_READ)
 
     def __iter__(self):
         return self
